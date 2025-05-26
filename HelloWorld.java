9653c2c2b42b98833fe82b6b664ea276177faa43
kaviyaP1105 <129420913+kaviyaP1105@users.noreply.github.com>
2025-05-26 10:37:26 +0530

    THIS IS MY FIRST TASK
    1. Hello World Program
    • Objective: Understand the basic structure of a Java program.
    • Task: Write a Java program that prints "Hello, World!" to the console.
    • Instructions:
    o Create a class named HelloWorld.
    o Implement the main method.
    o Use System.out.println() to display the message.

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+public class HelloWorld
+{
+	public static void main(String args[])
+	{
+		Scanner in=new Scanner(System.in);
+		System.out.println("Hello,World!");
+	}
+}
+		

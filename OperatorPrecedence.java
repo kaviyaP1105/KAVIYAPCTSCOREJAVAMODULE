5f6b562f536181a523535f5fe382c45be66a7549
kaviyaP1105 <129420913+kaviyaP1105@users.noreply.github.com>
2025-05-26 11:56:02 +0530

    TASK 8
    8. Operator Precedence
    • Objective: Explore how Java evaluates expressions.
    • Task: Evaluate and display the result of complex expressions.
    • Instructions:
    o Write expressions combining multiple operators, e.g., int result = 10 + 5 * 2;.
    o Display the result and explain the order of operations.

---
+++ OperatorPrecedence.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+class  OperatorPrecedence{
+    public static void main(String[] args) {
+        int result=10 + 5 * 2;
+        System.out.println(result);
+        System.out.println("First 5*2 is evaluated = "+(5*2));
+        System.out.println("Then 10 is added to 10 = "+(10+10));
+        
+    }
+}

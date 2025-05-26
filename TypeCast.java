de5525f39bb9b4bebc34e3314978f2ddf8f4598b
kaviyaP1105 <129420913+kaviyaP1105@users.noreply.github.com>
2025-05-26 11:22:40 +0530

    TASK 7
    7. Type Casting Example
    • Objective: Practice type casting between different data types.
    • Task: Convert a double to an int and vice versa.
    • Instructions:
    o Declare a double variable with a decimal value.
    o Cast it to an int and display the result.
    o Declare an int variable and cast it to a double, then display.

---
+++ TypeCast.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+class TypeCast{
+    public static void main(String[] args) {
+        double d=40.000000;
+        int doubleToInt=(int)(d);
+        System.out.println(doubleToInt);
+        int a=50;
+        double intToDouble=a;
+        System.out.println(intToDouble);
+        
+       
+    }
+}
